47b5ce45513cf948cb385580e8bc162e8a81e3e8
Philippe Schmouker <ph.schmouker@yahoo.fr>
2026-08-31 23:01:44 +0200

    #3-v0.1 - create c++ VS 2026 project
    Done. Verified.

---
+++ cpp/cpp.cpp
@@ -0,0 +1,9 @@
+// cpp.cpp : Defines the functions for the static library.
+//
+
+#include "framework.h"
+
+// TODO: This is an example of a library function, just used for testing first implementations
+void tests_cpp()
+{
+}
