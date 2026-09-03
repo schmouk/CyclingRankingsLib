51bd62300ff38085d8922d889135b450751aa1b8
Philippe Schmouker <ph.schmouker@yahoo.fr>
2026-09-03 12:19:14 +0200

    #27-fix copyright notices everywhere
    Completed.

--- crl-kotlin/commons/time.kt
+++ crl-kotlin/commons/time.kt
@@ -4,22 +4,24 @@ package com.github.schmouk.cyclingrankingslib
 * This file is part of project CyclingRankingsLibrary
 * (https://github.com/schmouk/CyclingRankingsLib)
 *
-* copyright (c) 2026 Philippe Schmouker
+* Copyright (C) 2026 Philippe Schmouker
 *
 * License is GNU GENERAL PUBLIC LICENSE Version 3
-* (full text available at:
-*  https://github.com/schmouk/CyclingRankingsLib?tab=GPL-3.0-1-ov-file)
-*/
-
-/**
-* This file is part of project CyclingRankingsLibrary
-* (https://github.com/schmouk/CyclingRankingsLib)
 *
-* copyright (c) 2026 Philippe Schmouker
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
 *
-* License is GNU GENERAL PUBLIC LICENSE Version 3
-* (full text available at:
-*  https://github.com/schmouk/CyclingRankingsLib?tab=GPL-3.0-1-ov-file)
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program (see:
+* https://github.com/schmouk/CyclingRankingsLib/blob/main/LICENSE).
+* If not, see <https://www.gnu.org/licenses/>.
 */
 
 //=====   Time Fraction of Seconds   ======================
